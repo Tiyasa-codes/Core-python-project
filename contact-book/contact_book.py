fe64fd3d80ae827e30bcbba1c50dc726d9998cc9
Tiyasa <t90218900@gmail.com>
2026-09-25 15:42:38 +0530

    add cd

--- contact-book/contact_book.py
+++ contact-book/contact_book.py
@@ -1,18 +1,12 @@
 contacts={}
 choice=int(input("enter your choice: 1 → Add Contact,2 → Search Contact,3 → Delete Contact,4 → View Contacts,5 → Exit:-"))
 while choice!=5:
- print("Your menu is:" \
- "1 → Add Contact," \
- "2 → Search Contact," \
- "3 → Delete Contact," \
- "4 → View Contacts," \
- "5 → Exit")
  if(choice==1):
   name=input("Enter your name:")
   phone=input("Enter your phone no:")
   contacts[name]=phone
  if(choice==2):
-  name=("enter the name you want to search:")
+  name=input("enter the name you want to search:")
   if(name in contacts):
    print(contacts[name])
   else:
